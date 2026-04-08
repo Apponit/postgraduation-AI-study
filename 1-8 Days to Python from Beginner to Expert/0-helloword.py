1c0c3eaae99ca687fa43ad1c40293833d6891dcf
Apponit <2633566067@qq.com>
2026-04-08 11:13:06 +0800

    python基础，基本数据类型+判断语句+循环语句

--- 1-8 Days to Python from Beginner to Expert/0-helloword.py
+++ 1-8 Days to Python from Beginner to Expert/0-helloword.py
@@ -1,1 +1,16 @@
+"""
+    单行注释
+    多行注释
+    基本数据类型
+
+"""
 print(f"hello word!")
+money=50
+print(f"当前剩余的钱{money}元")
+
+name="小明"
+age=18
+avg_salary=19283
+
+message="姓名：%s，年龄：%d，工资：%d"%(name,age,avg_salary)
+print(message)
